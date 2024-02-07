de9f911022d7243ed1a288c002aa2d190e6df629
AnmolBains123 <157437756+AnmolBains123@users.noreply.github.com>
2024-02-06 19:52:13 -0500

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,27 @@
+
+
+var greating = "Hello"; 
+
+var name = "5"; 
+var name = "Class!";
+
+alter (greating + name); 
+alert("Hello!"); 
+username= window.prompt("What is your name?")
+
+console.log(username)
+
+
+console.log("name"); 
+
+function changeBackground(color){
+
+    document.body.style.background = color
+}
+
+window.addEventListener.apply("load",function() {changeBackground('blue')})
+
+//Hello, this is just a testing line 
+/* and */ // will make anything between into just comments 
+
+
