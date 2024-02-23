1764c64a99d21f0201d8e2c8b4e37e77dbe5f8b3
AnmolBains123 <157437756+AnmolBains123@users.noreply.github.com>
2024-02-23 18:03:37 -0500

    Update Main2.js

--- Main2.js
+++ Main2.js
@@ -1,1 +1,1 @@
-<script> alert("Hello, World!") </script>
+<script> alert("Hello, World!") </script> //this line is what causes the alert notification, any text between the quotation marks will be displayed in the notification box when the website is opened
